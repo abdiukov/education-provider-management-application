0e959b6a753f93c9ad0d5de2e22adc21101243f7
a.abdiukov <a.abdiukov@gmail.com>
2021-02-10 19:10:01 +1100

    initial upload (layout only, no logic)

---
+++ TafeEnrolment/UI/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        //Navigation
+        private void NavigateTeacherInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NavigateCourseInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NavigateStudentInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
